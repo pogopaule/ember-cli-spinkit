99b70e0c897d4233326eb04c47b7512b2540faf0
pogopaule <pogopaule@gmx.de>
2014-09-06 23:07:47 +0200

    hook into ember-clis build process

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
+var path = require('path'),
+    fs = require('fs');
+
+function EmberCLISpinKit(project) {
+  this.project = project;
+  this.name = 'ember-cli-spinkit';
+}
+
 EmberCLISpinKit.prototype.treeFor = function treeFor(name) {
+  var treePath = path.join('node_modules', 'ember-cli-spinkit', 'trees', name);
+  if (fs.existsSync(treePath)) {
+    return unwatchedTree(treePath);
+  }
 };
 
 EmberCLISpinKit.prototype.included = function included(app) {
+  this.app = app;
+  this.app.import('vendor/ember-cli-spinkit/styles/styles.css');
 };
+
+function unwatchedTree(dir) {
+  return {
+    read: function() {
+      return dir;
+    },
+    cleanup: function() {}
+  };
+}
+
+module.exports = EmberCLISpinKit;
