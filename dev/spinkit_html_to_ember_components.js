b3f2664fbc09d9592fc4c54074768a361372c508
pogopaule <pogopaule@gmx.de>
2014-09-06 23:00:40 +0200

    script is no working
    main change is that css classes get prefixed to avoid name colissions

--- dev/spinkit_html_to_ember_components.js
+++ dev/spinkit_html_to_ember_components.js
@@ -3,39 +3,98 @@ var $ = require('cheerio'),
   path = require('path'),
   fs = require('fs'),
   mkdir = require('mkdirp'),
+  cssParser = require('css'),
+  _ = require('underscore'),
 
   fileOptions = {
     encoding: 'utf8'
   },
-  stylesFolder = '../styles-addon/components/',
-  templatesFolder = '../templates-addon/components/';;
+  stylesFolder = '../trees/vendor/ember-cli-spinkit/styles/',
+  templatesFolder = '../trees/templates/components/';
+
+
+
+
+
+
+
 
 createFolders();
+fs.unlink(stylesFolder + 'styles.css');
 
 var finder = find('./SpinKit/');
 
 finder.on('file', function(filePath, stat) {
   if (isHTML(filePath)) {
     fs.readFile(filePath, fileOptions, function(err, fileContent) {
-      $html = $.load(fileContent);
-      var css = $html('html head style').text();
-      var html = $html('html body').html();
-      save(css, html, getComponentName(filePath));
+      var $html = $.load(fileContent),
+          name = getComponentName(filePath),
+          css = $html('html head style').text(),
+          html = $html('html body').html();
+
+      appendFile(stylesFolder, 'styles', '.css', prefixSelectorsInCss(css, name))
+      writeFile(templatesFolder, name, '.hbs', prefixClassesInHtml(html, name));
     });
   }
 });
+
+
+
+function prefixClassesInHtml(html, prefix) {
+  var $html = $.load(html);
+  $html('[class]').each(function(i, elem) {
+    var $elem = $(elem);
+    $elem.attr('class', prefixClass($elem.attr('class'), prefix));
+  });
+  return $html.html();
+}
+
+function prefixClass(clazz, prefix) {
+  var classParts = clazz.split(' ');
+  var prefixedClassParts = _.map(classParts, function(part) {
+    return prefix + '-' + part;
+  });
+  return prefixedClassParts.join(' ');
+}
+
+function prefixSelectorsInCss(css, prefix) {
+  var parsedCss = cssParser.parse(css);
+  _.each(parsedCss.stylesheet.rules, function(item) {
+    if(item.type === 'rule') {
+      item.selectors = _.map(item.selectors, function(selector) {
+        return prefixSelector(selector, prefix);
+      });
+    }
+  });
+  return cssParser.stringify(parsedCss);
+}
+
+function prefixSelector(selector, prefix) {
+  var selectorParts = selector.split(' ');
+  var prefixedSelectorParts = _.map(selectorParts, function(part) {
+    if(part[0] === '.') {
+      return '.' + prefix + '-' + part.substring(1);
+    } else {
+      return part;
+    }
+  });
+  return prefixedSelectorParts.join(' ');
+}
 
 function isHTML(file) {
   return path.extname(file) === '.html';
 }
 
-function save(css, html, name) {
-  writeFile(stylesFolder, name, '.css', css);
-  writeFile(templatesFolder, name, '.hbs', html);
-}
-
 function writeFile(folder, fileName, extension, content) {
   fs.writeFile(folder + fileName + extension, content, fileOptions, function(error) {
+    if (error) {
+      throw error
+    }
+  });
+}
+
+function appendFile(folder, fileName, extension, content) {
+  fs.appendFile(folder + fileName + extension, content, fileOptions, function(error) {
     if (error) {
       throw error
     }
